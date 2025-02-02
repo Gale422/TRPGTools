bbac493547e9256e0bb9da522a044c5875d5e7b2
Gale <heartlessheartfez@gmail.com>
2025-02-02 09:30:59 +0900

    保存しているキャラクター名のレイアウトを変更

--- CharacterChoicer/main.js
+++ CharacterChoicer/main.js
@@ -4,7 +4,7 @@
     let num = document.querySelector('#choiceNum').value;
     num = parseInt(num);
     num = num && num > 1 ? num : '';
-    let nameList = Array.from(document.querySelectorAll('.charaCheck input[type=checkbox]:checked'), e => e.closest('tr')?.querySelector('input.charaName').value).filter(e => e);
+    let nameList = Array.from(document.querySelectorAll('#PLListTable .charaCheck input[type=checkbox]:checked, #OtherTable .charaCheck input[type=checkbox]:checked'), e => e.closest('tr')?.querySelector('input.charaName').value).filter(e => e);
     document.querySelector('#result').value = `choice${num}[${nameList.join(',')}]`;
   });
 
@@ -18,73 +18,6 @@
         },
           1000);
       });
-  });
-
-  // テーブル全体チェックボックスのセルをクリックした場合に、内部のチェックボックスの値を変更する
-  document.querySelectorAll('.tableCheck').forEach(e => {
-    e.addEventListener('click', event => {
-      const checkbox = event.currentTarget.querySelector('input[type=checkbox]');
-      checkbox.checked = !checkbox.checked;
-      checkbox.dispatchEvent(new Event('change'));
-    });
-  });
-
-  // テーブル全体チェックボックスの値が変更された場合の動作
-  document.querySelectorAll('.tableCheck input[type=checkbox]').forEach(e => {
-    e.addEventListener('click', event => {
-      // チェックボックスを直接クリックした場合は親要素へのイベントの伝播を止める
-      event.stopPropagation();
-    });
-    // チェックボックスの値が変更された場合はテーブル全体を変更する
-    e.addEventListener('change', event => {
-      const isChecked = event.currentTarget.checked;
-      const table = event.currentTarget.closest('table');
-      table.querySelectorAll('.charaCheck input[type=checkbox]').forEach(c => {
-        c.checked = isChecked;
-      });
-    });
-  });
-
-  // セル内をクリックした場合にも内部のチェックボックスの値を変更する
-  document.querySelectorAll('#PLListTable tbody,#OtherTable tbody').forEach(e => {
-    e.addEventListener('click', event => {
-      if (event.target && event.target.matches('.charaCheck')) {
-        const checkbox = event.target.querySelector('input[type=checkbox]');
-        checkbox.checked = !checkbox.checked;
-        checkbox.dispatchEvent(new Event('change'));
-      }
-    });
-  });
-
-  // プラスボタン
-  document.querySelectorAll('.place').forEach(e => {
-    e.addEventListener('click', event => {
-      const listNavi = event.currentTarget.closest('table').querySelector('tbody tr.listNavi');
-      // 新しい行を複製して表に挿入します。
-      const template = document.querySelector('#charaTemplate');
-      const clone = template.content.cloneNode(true);
-      if (listNavi) {
-        listNavi.before(clone);
-      } else {
-        event.currentTarget.closest('table').querySelector('tbody').append(clone);
-      }
-    });
-  });
-
-  // マイナスボタン
-  document.querySelectorAll('.minus').forEach(e => {
-    e.addEventListener('click', event => {
-      // 最後の行を削除します。
-      const listNavi = event.currentTarget.closest('table').querySelector('tbody tr.listNavi');
-      listNavi.previousElementSibling?.remove();
-    });
-  });
-
-  // 名称全削除ボタン
-  document.querySelectorAll('.nameDel').forEach(e => {
-    e.addEventListener('click', event => {
-      event.currentTarget.closest('table').querySelectorAll('.charaName')?.forEach(elm => elm.value = '');
-    });
   });
 
   const savedKey = 'savedCharaData';
@@ -114,12 +47,22 @@
       });
 
       for (const { name, group } of result) {
-        // 新しい行を複製して表に挿入します。
-        const template = document.querySelector('#savedCharaTemplate');
-        const clone = template.content.cloneNode(true);
-        clone.querySelector('.savedCharaGroup').textContent = group;
-        clone.querySelector('.savedCharaName').textContent = name;
-        list.appendChild(clone);
+        let table = Array.from(document.querySelectorAll('#savedCharaList .groupTable'))?.filter(e => e.querySelector('.groupName').textContent === group);
+        if (table.length < 1) {
+          // グループのテーブルが存在しないため、グループテーブルを追加
+          const tableTemplate = document.querySelector('#savedCharaTableTemplate');
+          const clone = tableTemplate.content.cloneNode(true);
+          clone.querySelector('.groupName').textContent = group;
+          list.appendChild(clone);
+          table = Array.from(document.querySelectorAll('#savedCharaList .groupTable'))?.filter(e => e.querySelector('.groupName').textContent === group);
+        }
+        {
+          // 対象グループのテーブルに行追加
+          const template = document.querySelector('#savedCharaTemplate');
+          const clone = template.content.cloneNode(true);
+          clone.querySelector('.savedCharaName').textContent = name;
+          table[0].querySelector('tbody').appendChild(clone);
+        }
       }
     }
   }
@@ -154,10 +97,63 @@
     });
   });
 
+
+  // イベント移譲系の処理登録(clickイベント)
   document.querySelector('body').addEventListener('click', event => {
-    // キャラ名追加
-    if (event.target && event.target.matches('button.addName')) {
-      const charaName = event.target.closest('.savedCharaData').querySelector('.savedCharaName')?.textContent;
+    if (!event.target) {
+      return;
+    }
+    const target = event.target;
+    if (target.matches('.tableCheck')) {
+      // テーブル全体チェックボックスのセルをクリックした場合に、内部のチェックボックスの値を変更する
+      const checkbox = target.querySelector('input[type=checkbox]');
+      checkbox.checked = !checkbox.checked;
+      checkbox.dispatchEvent(new Event('change', { bubbles: true }));
+      return;
+    } else if (target.matches('.tableCheck input[type=checkbox]')) {
+      // チェックボックスを直接クリックした場合は親要素へのイベントの伝播を止める
+      event.stopPropagation();
+      return;
+    } else if (target.matches('.charaCheck')) {
+      // セル内をクリックした場合にも内部のチェックボックスの値を変更する
+      const checkbox = target.querySelector('input[type=checkbox]');
+      checkbox.checked = !checkbox.checked;
+      checkbox.dispatchEvent(new Event('change', { bubbles: true }));
+      return;
+    } else if (target.matches('.place')) {
+      // プラスボタン
+      const listNavi = target.closest('table').querySelector('tbody tr.listNavi');
+      // 新しい行を複製して表に挿入します。
+      const template = document.querySelector('#charaTemplate');
+      const clone = template.content.cloneNode(true);
+      if (listNavi) {
+        listNavi.before(clone);
+      } else {
+        target.closest('table').querySelector('tbody').append(clone);
+      }
+      return;
+    } else if (target.matches('.minus')) {
+      // マイナスボタン
+      const listNavi = target.closest('table').querySelector('tbody tr.listNavi');
+      listNavi.previousElementSibling?.remove();
+      return;
+    } else if (target.matches('.nameDel')) {
+      // 名称全削除ボタン
+      target.closest('table').querySelectorAll('.charaName')?.forEach(elm => elm.value = '');
+      return;
+    } else if (target.matches('.groupName')) {
+      // グループ名をクリックするとテーブルの全選択チェックボックスを変動させる
+      const checkbox = target.closest('tr').querySelector('.tableCheck input[type=checkbox]');
+      checkbox.checked = !checkbox.checked;
+      checkbox.dispatchEvent(new Event('change', { bubbles: true }));
+    } else if (target.matches('.savedCharaName')) {
+      // 保存したキャラ名をクリックすると選択のチェックボックスを変動させる
+      const checkbox = target.closest('tr').querySelector('.charaCheck input[type=checkbox]');
+      checkbox.checked = !checkbox.checked;
+      checkbox.dispatchEvent(new Event('change', { bubbles: true }));
+    } else if (target.matches('#addName')) {
+      // 保存したキャラ名追加
+      const charaNames = Array.from(document.querySelectorAll('#savedCharaList tbody input[type=checkbox]:checked'), e => e.closest('tr').querySelector('.savedCharaName').textContent);
       {
         // 既存行に追加できるかどうかを確認
         const list = Array.from(document.querySelectorAll('#PLListTable tbody tr:not(.listNavi) input.charaName')).reverse();
@@ -169,28 +165,28 @@
             break;
           }
         }
-        if (index < 0) {
-          // 最終行が埋まっている -> 新規行の追加
-          const listNavi = document.querySelector('#PLListTable tbody tr.listNavi');
-          // 新しい行を複製して表に挿入します。
-          const template = document.querySelector('#charaTemplate');
-          const clone = template.content.cloneNode(true);
-          clone.querySelector('.charaName').value = charaName || '';
-          if (listNavi) {
-            listNavi.before(clone);
+        const listNavi = document.querySelector('#PLListTable tbody tr.listNavi');
+        for (const charaName of charaNames) {
+          if (index < 0) {
+            // 最終行が埋まっている -> 新規行の追加
+            const template = document.querySelector('#charaTemplate');
+            const clone = template.content.cloneNode(true);
+            clone.querySelector('.charaName').value = charaName || '';
+            if (listNavi) {
+              listNavi.before(clone);
+            } else {
+              document.querySelector('#PLListTable tbody').append(clone);
+            }
           } else {
-            document.querySelector('#PLListTable tbody').append(clone);
+            // 開いている行がある -> 該当行に追加
+            list[index--].value = charaName || '';
           }
-        } else {
-          // 開いている行がある -> 該当行に追加
-          list[index].value = charaName || '';
         }
       }
       return;
-    }
-    // キャラ名削除
-    if (event.target && event.target.matches('button.delName')) {
-      const charaName = event.target.closest('.savedCharaData').querySelector('.savedCharaName')?.textContent;
+    } else if (target.matches('.delName') || target.closest('td')?.matches('.delName')) {
+      // 保存したキャラ名削除
+      const charaName = target.closest('tr').querySelector('.savedCharaName')?.textContent;
       if (charaName) {
         let dataStr = localStorage.getItem(savedKey);
         if (!dataStr) {
@@ -203,6 +199,22 @@
         reload();
       }
       return;
+    }
+  });
+
+  // イベント移譲系の処理登録(changeイベント)
+  document.querySelector('body').addEventListener('change', event => {
+    if (!event.target) {
+      return;
+    }
+    const target = event.target;
+    if (target.matches('.tableCheck input[type=checkbox]')) {
+      // チェックボックスの値が変更された場合はテーブル全体を変更する
+      const isChecked = target.checked;
+      const table = target.closest('table');
+      table.querySelectorAll('.charaCheck input[type=checkbox]').forEach(c => {
+        c.checked = isChecked;
+      });
     }
   });
 
